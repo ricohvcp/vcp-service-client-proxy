4bb31488dfb418854dcdb3c32ca025f9ff7cdf5f
ysuke <yusuke-k@nts.ricoh.co.jp>
2015-05-12 11:57:33 +0900

    implement

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-var superagent-proxy = require('superagent-proxy');
+var superagent_proxy = require('superagent-proxy');
+var URL = require('url');
 
-module.exports = function(req) {
-  let url = URL.parse(req.url);
-  let proxy_url;
+module.exports = function(setting) {
+  setting = setting || {};
 
-  // select http/https proxy from env
-  // and adding port number to url for superagent-proxy
-  switch (url.protocol) {
-    case 'http:':
-      proxy_url = process.env.HTTP_PROXY;
-      if (url.port === null) {
-        url.port = '80';
-        url.host += ':80';
-      }
-      break;
-    case 'https:':
-      proxy_url = process.env.HTTPS_PROXY;
-      if (url.port === null) {
-        url.port = '443';
-        url.host += ':443';
-      }
-      break;
-    default:
-      throw new Error('unsupported protocol');
+  return function(req) {
+    var url = URL.parse(req.url);
+    var proxy_url;
+
+    // select http/https proxy from setting or env
+    // and adding port number to url for superagent-proxy
+    switch (url.protocol) {
+      case 'http:':
+        proxy_url = setting.http || process.env.HTTP_PROXY;
+        if (url.port === null) {
+          url.port = '80';
+          url.host += ':80';
+        }
+        break;
+      case 'https:':
+        proxy_url = setting.https || process.env.HTTPS_PROXY;
+        if (url.port === null) {
+          url.port = '443';
+          url.host += ':443';
+        }
+        break;
+      default:
+        throw new Error('unsupported protocol');
+    }
+
+    req.url = URL.format(url);
+    superagent_proxy(req, proxy_url);
   }
-
-  superagent-proxy(req, proxy_url);
 }
